9e4642cf9ad360846da7297533d8664d3c0652c9
Administrator <Administrator@MSDN-SPECIAL>
2016-08-11 11:27:00 +0900

    test4

--- gradle_sts/src/main/java/com/gradle/mvc/HomeController.java
+++ gradle_sts/src/main/java/com/gradle/mvc/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
      
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
-        logger.info("Welcome home! The client locale is {}....6", locale);
+        logger.info("Welcome home! The client locale is {}....7", locale);
          
         Date date = new Date();
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
